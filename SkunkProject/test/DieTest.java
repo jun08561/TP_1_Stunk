129467e52542222c0a1414212c72a0aee6ba1480
June <June@LAPTOP-O1SHBO3I>
2018-10-08 15:40:29 -0500

    This is a change

--- SkunkProject/test/DieTest.java
+++ SkunkProject/test/DieTest.java
@@ -4,12 +4,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.princeton.cs.introcs.StdOut;
+
 public class DieTest
 {
+	private Die die;
 
 	@Before
 	public void setUp() throws Exception
 	{
+		int[] init_values = new int[] {1,2,3,4};
+		die = new Die(init_values);
+		StdOut.println("setUp()");
 	}
 
 	@After
@@ -41,13 +47,23 @@ public class DieTest
 		
 	}
 	@Test
+	//this is the one successful dice test
 	public void test_dice() {
-		Dice dice= new Dice();
+		
+		int[] init_values =new int[] {6,7,8};
+	    Die die2 = new Die(init_values);
+	    int[] init_values1 =new int[] {3,2,1};
+	    Die die = new Die(init_values1);
+	    Dice dice= new Dice(die, die2);
 		dice.roll();
 		int result = dice.getLastRoll();
 		assertEquals(9, result);
 		
 	}
+	
+
+	
+	
 
 		
 		
